c80bccb7156aa4877e3033ea983992f7e47e9013
Vlados <vnegoluk@gmail.com>
2024-08-07 17:00:48 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,24 @@
+"use strict";
+const headers = document.querySelectorAll("th");
+const tableBody = document.querySelector("table tbody");
+const rows = [
+    ...tableBody.querySelectorAll("tr")
+];
+headers.forEach((header, index)=>{
+    header.addEventListener("click", ()=>{
+        sortTable(index);
+    });
+});
+function sortTable(columnIndex) {
+    rows.sort((rowA, rowB)=>{
+        const cellA = rowA.cells[columnIndex].textContent;
+        const cellB = rowB.cells[columnIndex].textContent;
+        return cellA.localeCompare(cellB, undefined, {
+            numeric: true
+        });
+    });
+    tableBody.innerHTML = "";
+    rows.forEach((row)=>tableBody.appendChild(row));
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
